20943bbb1ec2aae89e392ae29be4e09622f1118d
Michael Haberler <git@mah.priv.at>
2013-08-18 07:34:33 +0200

    src/hal/drivers/mesa-hostmot2/hostmot2.c changes

--- src/hal/drivers/mesa-hostmot2/hostmot2.c
+++ src/hal/drivers/mesa-hostmot2/hostmot2.c
@@ -127,7 +127,7 @@ static void hm2_write(void *void_hm2, long period) {
     //UARTS need to be explicity handled by an external component
     hm2_tram_write(hm2);
 
-    // these usually do nothing
+    //these usually do nothing
     // they only write to the FPGA if certain pins & params have changed
     hm2_ioport_write(hm2);    // handles gpio.is_output but not gpio.out (that's done in tram_write() above)
     hm2_watchdog_write(hm2);  // in case the user has written to the watchdog.timeout_ns param
@@ -481,16 +481,17 @@ static void hm2_print_idrom(hostmot2_t *hm2) {
 
 
 static int hm2_read_idrom(hostmot2_t *hm2) {
+    u32 idrom;
 
     //
     // find the idrom offset
     //
 
-    if (!hm2->llio->read(hm2->llio, HM2_ADDR_IDROM_OFFSET, &hm2->idrom_offset, 2)) {
+    if (!hm2->llio->read(hm2->llio, HM2_ADDR_IDROM_OFFSET, &idrom, 4)) {
         HM2_ERR("error reading IDROM Offset\n");
         return -EIO;
     }
-
+    hm2->idrom_offset = (u16) idrom;
 
     //
     // first read in the idrom type to make sure we know how to deal with it
